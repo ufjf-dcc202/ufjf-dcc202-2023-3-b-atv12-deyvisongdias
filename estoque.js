f76ce3835ef4c44eb383f4841d844a669c38ac34
deyvisongdias <deyvisondias@hotmail.com>
2023-12-10 17:37:13 -0300

    Corrigindo codigo no estoque.js e main.js

--- estoque.js
+++ estoque.js
@@ -1,82 +1,88 @@
-
-
 let estoque = {
     'joao': [
-        { 'tipo': 'maca', 'qtd': 1 },
-        { 'tipo': 'pera', 'qtd': 2 }
+        { 'tipo': 'maca', 'quantidade': 1 },
     ],
     'maria': [
-        { 'tipo': 'maca', 'qtd': 2 },
-        { 'tipo': 'banana', 'qtd': 4 }
-    ]
+        { 'tipo': 'maca', 'quantidade': 2 },
+    ],
 };
 
 function getEstoque() {
     return structuredClone(estoque);
 }
 
-function transacao(origem, destino, tipo, quantidade) {
-    if (origem === destino) {
+function transacaoNoEstoque(origem, destino, tipo, quantidade) {
+
+    if (!estoque[origem] && origem !== "pomar") {
+        estoque[origem] = [];
+    }
+
+    if (!estoque[destino] && destino !== "pomar") {
+        estoque[destino] = [];
+    }
+
+    if (quantidade < 0 || origem === destino) {
         return;
     }
+
     if (destino === "pomar") {
-        dePessoaParaPomar(origem, tipo, quantidade);
+        let itemEncontrado = estoque[origem].find(item => item.tipo === tipo);
+
+        if (itemEncontrado) {
+            if (itemEncontrado.quantidade >= quantidade) {
+                itemEncontrado.quantidade = itemEncontrado.quantidade - quantidade;
+            } else {
+                itemEncontrado.quantidade = 0;
+            }
+        } else {
+            return;
+        }
         return;
     }
 
-    const pessoaOrigem = estoque[origem];
-    const pessoaDestino = estoque[destino];
-    let monteOrigem;
-    for (let i = 0; i < pessoaOrigem.length; i++) {
-        const monte = pessoaOrigem[i];
-        if (monte.tipo === tipo) {
-            monteOrigem = monte;
-            break;
+
+    if (origem === "pomar") {
+        const itemEncontrado = estoque[destino].find(item => item.tipo === tipo);
+
+        if (itemEncontrado) {
+            itemEncontrado.quantidade += quantidade;
+        } else {
+            estoque[destino].push({ tipo, quantidade });
         }
-    }
-    if (!monteOrigem) {
         return;
     }
-    let monteDestino;
-    for (let i = 0; i < pessoaDestino.length; i++) {
-        const monte = pessoaDestino[i];
-        if (monte.tipo === tipo) {
-            monteDestino = monte;
-            break;
-        }
-    }
-    if (!monteDestino) {
-        monteDestino = { 'tipo': tipo, 'qtd': 0 };
-        pessoaDestino.push(monteDestino);
-    }
-    const qtdReal = Math.min(quantidade, monteOrigem.qtd);
-    monteDestino.qtd += qtdReal;
-    monteOrigem.qtd -= qtdReal;
-}
 
-function dePessoaParaPomar(origem, tipo, quantidade) {
-    const pessoa = estoque[origem];
-    for (let i = 0; i < estoque[origem].length; i++) {
-        const monte = pessoa[i];
-        if (monte.tipo === tipo) {
-            monte.qtd -= Math.min(quantidade, monte.qtd);
+    else {
+        let itemOrigem = estoque[origem].find(item => item.tipo === tipo);
+        let itemDestino = estoque[destino].find(item => item.tipo === tipo);
+
+        if (!itemOrigem) {
             return;
         }
-    }
-}
 
+        else if (itemOrigem.quantidade < quantidade) {
+            if (itemDestino) {
+                itemDestino.quantidade += itemOrigem.quantidade;
+            } else {
+                estoque[destino].push({ tipo: tipo, quantidade: itemOrigem.quantidade });
+            }
+            itemOrigem.quantidade = 0;
+        }
 
-function dePomarParaPessoa(destino, tipo, quantidade) {
-    const pessoa = estoque[destino];
-    for (let i = 0; i < pessoa.length; i++) {
-        const monte = pessoa[i];
-        if (monte.tipo === tipo) {
-            monte.qtd += Math.max(quantidade, 0);
-            return;
+        else {
+            if (itemDestino) {
+                itemDestino.quantidade += quantidade;
+            } else {
+                estoque[destino].push({ tipo, quantidade });
+            }
+            itemOrigem.quantidade = itemOrigem.quantidade - quantidade;
         }
     }
-    const novoMonte = { 'tipo': tipo, 'qtd': Math.max(quantidade, 0) };
-    pessoa.push(novoMonte);
+    return;
 }
 
-export { getEstoque, transacao };
+function limpaEstoque() {
+    estoque = {};
+}
+
+export { getEstoque, transacaoNoEstoque, limpaEstoque };
--- main.js
+++ main.js
@@ -1,38 +1,53 @@
-import { getEstoque, transacao } from "./estoque.js";
+import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
 
-const olJoao = document.querySelector("#joao")
-const olMaria = document.querySelector("#maria")
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
 
-document.entrada.addEventListener('submit', leFormulario)
+document.entrada.addEventListener('submit', leFormulario);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
 
 atualizaTela();
 
 function leFormulario(event) {
-    event.preventDefault();
+    event.preventDefault(event);
     const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
     const destino = document.entrada.destino.value;
 
-    console.log(`${origem} de ${quantidade} ${fruta} para ${destino}`)
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 
-    transacao(origem, destino, fruta, quantidade);
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
     atualizaTela();
-    //document.entrada.submit();
 }
 
-function atualizaTela() {
-    const estoque = getEstoque();
-    preencheLista(olJoao, estoque.joao);
-    preencheLista(olMaria, estoque.maria);
-}
 
 function preencheLista(lista, estoqueDaPessoa) {
-    lista.innerHTML = "";
+    lista.textContent = "";
+
     for (let i = 0; i < estoqueDaPessoa.length; i++) {
         const monte = estoqueDaPessoa[i];
         const li = document.createElement('li');
-        li.textContent = `${monte.tipo}: ${monte.qtd}`;
-        lista.append(li);
+        li.textContent = `${monte.tipo}: ${monte.quantidade}`;
+        lista.appendChild(li);
     }
+}
+
+
+function atualizaTela() {
+    const estoque = getEstoque();
+
+    olJoao.innerHTML = "";
+    olMaria.innerHTML = "";
+    document.entrada.quantidade.value = 1;
+    document.entrada.fruta.value = "maca";
+
+    preencheLista(olJoao, estoque.joao);
+    preencheLista(olMaria, estoque.maria);
 }
